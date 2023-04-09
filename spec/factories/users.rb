c97b44f8da98a7ad332a648952fcad39cb3f6280
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-04-10 01:33:47 +0900

    add システムスペックusers_specを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     sequence(:name) { |n| "User #{n}" }
     sequence(:email) { |n| "user#{n}@example.com" }
     password { "password" }
+    profile { "My profile" }
+    after(:create) do |user|
+      user.avatar.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'images', 'avatar.png')), filename: 'avatar.png',
+                         content_type: 'image/png')
+    end
   end
 end
