96603b12a14b215c03bb9ad8e4355978ae4a40f1
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-04-10 08:29:55 +0900

    add リクエストスペックusers_specを編集

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:user) { create(:user) }
+
   describe "GET /show" do
+    before { sign_in user }
+
     it "returns http success" do
-      get "/users/show"
+      get user_path(user)
       expect(response).to have_http_status(:success)
     end
   end
