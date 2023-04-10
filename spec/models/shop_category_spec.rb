e8476a27ba3a9fddd2090e0c5c4f2e6931865b17
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-04-10 09:51:37 +0900

    add モデルスペックshop_category_specを追加

--- spec/models/shop_category_spec.rb
+++ spec/models/shop_category_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe ShopCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:user) { create(:user) }
+    let(:shop) { create(:shop, user: user) }
+    let(:category) { create(:category) }
+    let!(:shop_category) { create(:shop_category, shop: shop, category: category) }
+
+    it 'is valid with valid attributes' do
+      expect(shop_category).to be_valid
+    end
+
+    it 'is invalid without a shop_id' do
+      shop_category.shop_id = nil
+      expect(shop_category).not_to be_valid
+    end
+
+    it 'is invalid without a category_id' do
+      shop_category.category_id = nil
+      expect(shop_category).not_to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'is associated with Shop' do
+      association = described_class.reflect_on_association(:shop)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'is associated with category' do
+      association = described_class.reflect_on_association(:category)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
 end
