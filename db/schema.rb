5e9485af76fc902abddc845951b70facca0c8935
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-04-10 15:45:23 +0900

    add Bookmarkモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_035017) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_064436) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_035017) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "shop_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_bookmarks_on_shop_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -97,6 +106,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_035017) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookmarks", "shops"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "comments", "shops"
   add_foreign_key "comments", "users"
   add_foreign_key "shop_categories", "categories"
