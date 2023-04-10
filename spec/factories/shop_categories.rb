e90117d8411e35994d1c525c3f3ada6fd7b86774
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-04-10 09:46:18 +0900

    add モデルスペックcategory_specを編集

--- spec/factories/shop_categories.rb
+++ spec/factories/shop_categories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :shop_category do
-    shop { nil }
-    category { nil }
+  factory :shop_category, class: "ShopCategory" do
+    shop_id { create(:shop).id }
+    category_id { create(:category).id }
   end
 end
