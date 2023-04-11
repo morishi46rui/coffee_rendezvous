e80197867e3603afbc67515e0a1372d49e2862cc
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-04-11 23:55:58 +0900

    fix レイアウトの修正

--- spec/system/layouts_spec.rb
+++ spec/system/layouts_spec.rb
@@ -20,44 +20,43 @@ RSpec.describe "Layouts", type: :system do
     it "displays the Search link" do
       expect(page).to have_link("Search", href: shops_path, class: "nav-link")
     end
-    
+
     context "when the user is signed in" do
-    
       before do
         sign_in user
       end
-    
+
       it "displays the user's name in the dropdown menu" do
         expect(page).to have_css(".dropdown-toggle", text: user.name)
       end
-    
+
       it "displays the user's profile link in the dropdown menu" do
         expect(page).to have_link("マイページ", href: user_path(user.id), class: "dropdown-item")
       end
-    
+
       it "displays the logout link in the dropdown menu" do
         expect(page).to have_link("ログアウト", href: destroy_user_session_path, class: "dropdown-item")
       end
     end
-    
+
     context "when the user is not signed in" do
       it "displays Guest in the dropdown menu" do
         expect(page).to have_css(".dropdown-toggle", text: "Guest")
       end
-    
+
       it "displays the login link in the dropdown menu" do
         expect(page).to have_link("ログイン", href: new_user_session_path, class: "dropdown-item")
       end
-    
+
       it "displays the sign up link in the dropdown menu" do
         expect(page).to have_link("新規登録", href: new_user_registration_path, class: "dropdown-item")
       end
-    
+
       it "displays the guest sign in link in the dropdown menu" do
         expect(page).to have_link("ゲストログイン", href: "/guest_sign_in", class: "dropdown-item")
       end
     end
-    
+
     it "displays the Instagram icon with the correct link" do
       within "nav" do
         expect(page).to have_link(href: "https://www.instagram.com/coffee_rendez_vous/")
