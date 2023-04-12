cf9ebfc66c82ecfbf59044dbff126ef99094952e
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-04-12 22:24:10 +0900

    fix 軽微な修正

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe "Comments", type: :system do
     visit shop_path(shop)
   end
 
-  it "コメントエリアが表示されていること" do
+  it "displays the comment area" do
     expect(page).to have_selector ".comment-area", count: shop.comments.count
   end
 
-  it "コメントフォームが表示されていること" do
+  it "displays the comment form" do
     expect(page).to have_content "コメントを投稿する"
     expect(page).to have_selector "form"
   end
 
-  it "コメント送信ボタンが表示されていること" do
+  it "displays the comment submit button" do
     expect(page).to have_button "送信"
   end
 end
