479f6a20278709ce5080b049bf8c468e1ed3d02f
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-04-12 22:24:42 +0900

    add モデルスペックuser_specを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let!(:user) { create(:user) }
+
+    it "is valid with the correct attributes" do
+      expect(user).to be_valid
+    end
+
+    it "is invalid without an email" do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+
+    it "is invalid without a name" do
+      user.name = nil
+      expect(user).not_to be_valid
+    end
+
+    it "is invalid with a short password" do
+      user.password = '12345'
+      expect(user).not_to be_valid
+    end
+  end
 end
